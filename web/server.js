ce54769a5f523640fb60ae59bcf60fe25b17f500
Husseinm1 <husseinm@deakin.edu.au>
2020-09-11 00:09:35 +1000

    Updated version

--- web/server.js
+++ web/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {  res.sendFile(`${base}/home.html`);});
+app.get('/', function (req, res) {  res.sendFile(`${base}/index.html`);});
 const port = 3000;
 const base = `${__dirname}/car`;
 app.listen(port, () => {console.log(`listening on port ${port}`);});
